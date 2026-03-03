5efe3981d76a6821157ba2b6778b33021ed77553
BollettinoAndrea <160122173+BollettinoAndrea@users.noreply.github.com>
2026-03-03 10:49:30 +0100

    Update index.php

--- wsphp/index.php
+++ wsphp/index.php
@@ -1,101 +1,115 @@
 <?php
+session_start();
 
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Type, Accept");
 
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    http_response_code(200);
-    exit();
+$method = $_SERVER['REQUEST_METHOD'];
+$contentType = $_SERVER['CONTENT_TYPE'] ?? "application/json";
+$accept = $_SERVER['HTTP_ACCEPT'] ?? "application/json";
+
+if (!isset($_SESSION['utenti'])) {
+    $_SESSION['utenti'] = [
+        ["id" => 1, "nome" => "Mario"],
+        ["id" => 2, "nome" => "Luca"]
+    ];
 }
 
-$method = $_SERVER["REQUEST_METHOD"];
-$contentType = $_SERVER["CONTENT_TYPE"] ?? "application/json";
-$accept = $_SERVER["HTTP_ACCEPT"] ?? "application/json";
+function sendResponse($data, $statusCode = 200) {
+    global $accept;
+
+    http_response_code($statusCode);
+
+    if ($accept === "application/xml") {
+        header("Content-Type: application/xml");
+        $xml = new SimpleXMLElement("<response/>");
+
+        foreach ($data as $key => $value) {
+            if (is_array($value)) {
+                $item = $xml->addChild("item");
+                foreach ($value as $k => $v) {
+                    $item->addChild($k, $v);
+                }
+            } else {
+                $xml->addChild($key, $value);
+            }
+        }
+
+        echo $xml->asXML();
+    } else {
+        header("Content-Type: application/json");
+        echo json_encode($data, JSON_PRETTY_PRINT);
+    }
+    exit;
+}
 
 $input = file_get_contents("php://input");
 
-$database = [
-    1 => ["id" => 1, "nome" => "Mario", "valore" => 100],
-    2 => ["id" => 2, "nome" => "Luigi", "valore" => 200]
-];
-
-function sendResponse($data, $accept) {
-
-    if (strpos($accept, "xml") !== false) {
-        header("Content-Type: application/xml");
-
-        $xml = new SimpleXMLElement("<response/>");
-        array_to_xml($data, $xml);
-        echo $xml->asXML();
-
-    } else {
-        header("Content-Type: application/json");
-        echo json_encode($data);
-    }
-
-    exit();
-}
-
-function array_to_xml($data, &$xml) {
-    foreach ($data as $key => $value) {
-        if (is_array($value)) {
-            $subnode = $xml->addChild(is_numeric($key) ? "item" : $key);
-            array_to_xml($value, $subnode);
-        } else {
-            $xml->addChild(is_numeric($key) ? "item" : $key, htmlspecialchars($value));
-        }
-    }
-}
-
-function getInputData($input, $contentType) {
-
-    if (strpos($contentType, "xml") !== false) {
-        $xml = simplexml_load_string($input);
-        return json_decode(json_encode($xml), true);
-    }
-
-    return json_decode($input, true);
+if ($contentType === "application/xml" && !empty($input)) {
+    $inputData = json_decode(json_encode(simplexml_load_string($input)), true);
+} else {
+    $inputData = json_decode($input, true);
 }
 
 switch ($method) {
 
     case "GET":
-        sendResponse($database, $accept);
+        sendResponse([
+            "status" => "success",
+            "data" => $_SESSION['utenti']
+        ]);
         break;
 
     case "POST":
-        $data = getInputData($input, $contentType);
-        $newId = rand(3, 999);
-        $data["id"] = $newId;
+        if (!$inputData || !isset($inputData['nome'])) {
+            sendResponse(["error" => "Dati non validi"], 400);
+        }
+
+        $newId = end($_SESSION['utenti'])['id'] + 1;
+        $nuovo = [
+            "id" => $newId,
+            "nome" => $inputData['nome']
+        ];
+
+        $_SESSION['utenti'][] = $nuovo;
 
         sendResponse([
-            "messaggio" => "Record creato",
-            "record" => $data
-        ], $accept);
+            "status" => "Creato",
+            "data" => $nuovo
+        ], 201);
         break;
 
     case "PUT":
-        $data = getInputData($input, $contentType);
+        if (!$inputData || !isset($inputData['id'])) {
+            sendResponse(["error" => "ID mancante"], 400);
+        }
 
-        sendResponse([
-            "messaggio" => "Record modificato",
-            "record" => $data
-        ], $accept);
+        foreach ($_SESSION['utenti'] as &$utente) {
+            if ($utente['id'] == $inputData['id']) {
+                $utente['nome'] = $inputData['nome'];
+                sendResponse(["status" => "Aggiornato", "data" => $utente]);
+            }
+        }
+
+        sendResponse(["error" => "Utente non trovato"], 404);
         break;
 
     case "DELETE":
-        $data = getInputData($input, $contentType);
+        if (!$inputData || !isset($inputData['id'])) {
+            sendResponse(["error" => "ID mancante"], 400);
+        }
 
-        sendResponse([
-            "messaggio" => "Record eliminato",
-            "id" => $data["id"] ?? null
-        ], $accept);
+        foreach ($_SESSION['utenti'] as $key => $utente) {
+            if ($utente['id'] == $inputData['id']) {
+                unset($_SESSION['utenti'][$key]);
+                sendResponse(["status" => "Eliminato"]);
+            }
+        }
+
+        sendResponse(["error" => "Utente non trovato"], 404);
         break;
 
     default:
-        http_response_code(405);
-        sendResponse(["errore" => "Metodo non consentito"], $accept);
-        
+        sendResponse(["error" => "Metodo non supportato"], 405);
 }
-?>
